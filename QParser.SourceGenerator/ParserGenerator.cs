4d933ffee18ad8a14dfb505167923ff72ddc8713
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2023-08-10 16:07:32 +0800

    Add parser generator project
    Signed-off-by: QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>

---
+++ QParser.SourceGenerator/ParserGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace QParser.SourceGenerator;
+
+[Generator]
+public class ParserGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var grammarFiles = context.AdditionalFiles.Where(f => f.Path.EndsWith(".qg")).ToList();
+    }
+}
